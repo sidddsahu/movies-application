bb3bc683f13427190de68af7fe63a6b407514081
Siddhant sahu <114759848+sidddsahu@users.noreply.github.com>
2023-04-17 08:04:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
- 
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import MovieList from './components/movieList/movieList';
 import Movie from './pages/movieDetail/movie';
+import { Route, Routes } from 'react-router';
 function App() {
   return (
     <div className="App">
-      <Router>
+
         <Header/>
         <Routes>
           <Route index element={<Home/>}></Route>
           <Route path='movie/:id' element={<Movie/>}></Route>
           <Route path='movies/:type' element={<MovieList/>}></Route>
         </Routes>
-      </Router>
+     
     </div>
   );
 }
